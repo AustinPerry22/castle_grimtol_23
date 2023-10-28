64c1e4573a506fdc27e118206a518263584231ef
DJ Aaron <koreangeekman@gmail.com>
2023-10-28 15:12:41 -0600

    mobile ready char select

--- HalloweenHackathon.client/src/pages/CharacterSelect.vue
+++ HalloweenHackathon.client/src/pages/CharacterSelect.vue
@@ -1,50 +1,50 @@
 <template>
-  <div v-if="!player.character" class="container-fluid charSelectBG p-3">
+  <div v-if="!player.character" class="container-fluid charSelectBG p-2 p-md-3">
     <section class="row">
       <div class="col-12">
         <p class="fs-1 metalMania text-center">Select your character</p>
       </div>
     </section>
     <section v-for="char in characters" :key="char.id" @click="selectCharacter(char)" 
-      class="row border rounded p-3 m-3 justify-content-center">
-      <div class="col-12 col-md-2 p-3 d-flex justify-content-end">
+      class="row border rounded p-1 p-md-3 m-1 m-md-3 justify-content-center">
+      <div class="col-12 col-md-2 p-1 p-md-3 d-flex justify-content-end">
         <img :src="char.picture" :alt="char.name" class="rounded">
       </div>
       <div class="col-12 col-md-6 p-3 d-flex align-items-center">
-        <p class="robotoMono p-3 m-3 rounded bgBlur">{{ char.description }}</p>
+        <p class="robotoMono p-1 p-md-3 m-1 m-md-3 rounded bgBlur">{{ char.description }}</p>
       </div>
-      <div class="col-12 col-md-2 p-3 d-flex justify-content-start">
+      <div class="col-12 col-md-2 p-1 p-md-3 d-flex justify-content-start">
         <img :src="char.altPicture" :alt="char.altName" class="rounded">
       </div>
     </section>
   </div>
-  <div v-else class="container-fluid charSelectBG p-3">
-    <section class="row p-5">
+  <div v-else class="container-fluid charSelectBG p-1 p-md-3">
+    <section class="row p-2 p-md-5">
         <div class="col-12">
           <p class="fs-1 metalMania text-center mt-3">You have selected:</p>
         </div>
       </section>
-    <section class="row border rounded p-3 m-3 justify-content-center">
+    <section class="row border rounded p-1 p-md-3 m-1 m-md-3 justify-content-center">
       <div class="col-12 col-md-2 p-3 d-flex justify-content-end">
         <img :src="player.character.picture" :alt="player.character.name" class="rounded">
       </div>
-      <div class="col-12 col-md-6 p-3 d-flex align-items-center">
-        <p class="robotoMono p-3 m-3 rounded bgBlur">{{ player.character.description }}</p>
+      <div class="col-12 col-md-6 p-1 p-md-3 d-flex align-items-center">
+        <p class="robotoMono p-1 p-md-3 m-1 m-md-3 rounded bgBlur">{{ player.character.description }}</p>
       </div>
-      <div class="col-12 col-md-2 p-3 d-flex justify-content-start">
+      <div class="col-12 col-md-2 p-1 p-md-3 d-flex justify-content-start">
         <img :src="player.character.altPicture" :alt="player.character.altName" class="rounded">
       </div>
     </section>
-    <section class="row p-5">
+    <section class="row p-2 p-md-5">
         <div class="col-12 text-center">
           <router-link :to="{name:'Map'}">
             <button class="btn border-danger fs-1 metalMania text-center mt-3">Are you ready?</button>
           </router-link>
+          <div class="backButton pt-2 pt-md-5" @click="unselectCharacter()">
+            <i class="fs-1 text-danger mdi mdi-backspace"></i>
+          </div>
         </div>
       </section>
-    </div>
-    <div class="backButton" @click="unselectCharacter()">
-      <i class="fs-1 text-danger mdi mdi-backspace"></i>
     </div>
 </template>
 
@@ -121,8 +121,8 @@ img{
 
 .backButton{
   position: absolute;
-  top: 1rem;
-  left:1rem;
+  top: 1.666rem;
+  left:2rem;
   opacity: .666;
 }
 </style>
