8137239a4a57b3237ecfa278414c39507c6c219e
Tyler Marcott <tylerjmarcott@gmail.com>
2023-10-28 11:20:26 -0600

    making landing page

--- HalloweenHackathon.client/src/pages/HomePage.vue
+++ HalloweenHackathon.client/src/pages/HomePage.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 bg-white rounded elevation-3">
-      <img src="https://bcw.blob.core.windows.net/public/img/8600856373152463" alt="CodeWorks Logo"
-        class="rounded-circle">
-      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        Vue 3 Starter
-      </h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 text-center">
+        <h1>
+          Project Name Here
+        </h1>
+      </div>
+    </div>
+    <div class="row">
+      Welcome..... to the SPOOKIEST game of this year's Halloween! Be 
     </div>
   </div>
 </template>
