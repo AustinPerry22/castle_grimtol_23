bf31459c7f778c7f03446a67c577152c735f13d1
DJ Aaron <koreangeekman@gmail.com>
2023-10-28 11:19:49 -0600

    character array in appstate

---
+++ HalloweenHackathon.client/src/pages/CharacterSelect.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="container-fluid">
+  <section v-for="char in characters" :key="char.id" class="row">
+    <div class="col-12 col-md-4">
+      <img :src="char.image" :alt="char.name">
+    </div>
+    <div class="col-12 col-md-8">
+      <p>{{ char.description }}</p>
+    </div>
+  </section>
+</div>
+</template>
+
+
+<script>
+import { AppState } from '../AppState';
+import { computed, onMounted } from 'vue';
+
+export default {
+  setup(){
+  return {  }
+  }
+};
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
