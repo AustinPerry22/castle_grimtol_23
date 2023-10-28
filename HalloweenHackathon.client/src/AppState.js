5daa836c58f13ab390d28d04fccdf437dc22d271
DJ Aaron <koreangeekman@gmail.com>
2023-10-28 13:57:32 -0600

    tweaks on char select

--- HalloweenHackathon.client/src/AppState.js
+++ HalloweenHackathon.client/src/AppState.js
@@ -6,51 +6,65 @@ export const AppState = reactive({
   /** @type {import('./models/Account.js').Account} */
   account: {},
 
+  player: {
+    name: '',
+    character: '',
+    score: 0
+  },
+
   characters: [
     {
       name: "Queen of Grimtol",
+      altName: "Vampire Queen",
       description: "Closest to the sickly king, she had a large role in the kingdoms flourishing educational institutions. Now leading the kingdom through it's eternal tragedy",
       picture: '../assets/img/characters/Queen.jpg',
       altPicture: '../assets/img/characters/QueenVamp.png'
     },
     {
       name: "King of Grimtol",
+      altName: "Vampire King",
       description:"The beloved king, cursed with the eternal life he was promised, but at the cost of eternal hunger for the blood of his subjects loved...",
       picture: '../assets/img/characters/King.jpg',
       altPicture: '../assets/img/characters/KingVampire.png'
     },
     {
       name: "Humble Baker",
+      altName: "Frog Baker",
       description:"Known for their great baked goods, they were invited many times to the castle for feasts and celebrations... until all his bread was cursed with rot, maggots and flies",
       picture: '../assets/img/characters/Baker.jpg',
       altPicture: '../assets/img/characters/FrogBaker.png'
     },
     {
       name: "The Most Loyal Knight",
+      altName: "The Rat Knight",
       description:"Their loyalty to the city was and the law was honored above all else. Even when he discovered his partner had been stealing coin for the poor.",
       picture: '../assets/img/characters/Knight.jpg',
       altPicture: '../assets/img/characters/RatKnight.png'
     },
     {
       name: "Magic Merchant",
+      altName: "Mummy Merchant",
       description:"Known by may names and faces, this merchant specialized in goods to rejuvenate the body and mind. Unfortunately the products sold were more akin to a snake oils than restorative serums.",
       picture: '../assets/img/characters/Merchant.jpg',
       altPicture: '../assets/img/characters/MummyMerchant.png'
     },
     {
       name: "Mason's Apprentice",
+      altName: "Medusa Mason",
       description:"With skills comparable to her master at half the age, she was cursed before many to live in her master's shadow. At least now when people see her, they will know her stonework.",
       picture: '../assets/img/characters/Mason.jpg',
       altPicture: '../assets/img/characters/MedusaMason.png'
     },
     {
       name: "Spirit Healer",
+      altName: "Ghost Healer",
       description:"Not a resident of the city, this renowned healer traveled there for the kings bounty. Unfortunately she did not arrive before the stranger. Even more unfortunate that she took a liking to the city and wished not to leave.",
       picture: '../assets/img/characters/Healer.jpg',
       altPicture: '../assets/img/characters/GhostHealer.png'
     },
     {
       name: "Wise Wizard",
+      altName: "Zombie Wizard",
       description:"Once a seeker of knowledge, gathering the greatest minds the realm offered to the city. Now reduced to a crude creature searching for a different form of brain power.",
       picture: '../assets/img/characters/Wizard.jpg',
       altPicture: '../assets/img/characters/ZombieWizard.png'
--- HalloweenHackathon.client/src/pages/CharacterSelect.vue
+++ HalloweenHackathon.client/src/pages/CharacterSelect.vue
@@ -1,11 +1,14 @@
 <template>
-<div class="container-fluid">
+<div class="container-fluid charSelectBG">
   <section v-for="char in characters" :key="char.id" class="row">
-    <div class="col-12 col-md-4">
-      <img :src="char.image" :alt="char.name">
+    <div class="col-12 col-md-3">
+      <img :src="char.picture" :alt="char.name">
     </div>
-    <div class="col-12 col-md-8">
-      <p>{{ char.description }}</p>
+    <div class="col-12 col-md-6">
+      <p class="robotoMono p-3 m-3 rounded bgBlur">{{ char.description }}</p>
+    </div>
+    <div class="col-12 col-md-3">
+      <img :src="char.altPicture" :alt="char.altName">
     </div>
   </section>
 </div>
@@ -17,13 +20,51 @@ import { AppState } from '../AppState';
 import { computed, onMounted } from 'vue';
 
 export default {
-  setup(){
-  return {  }
+  setup() {
+    
+    return { 
+      characters: AppState.characters,
+    
+    }
   }
 };
 </script>
 
 
 <style lang="scss" scoped>
+@charset "UTF-8";
+@import"https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,400;0,600;1,400&family=Metal+Mania&family=VT323&display=swap";
+@font-face {
+    font-family: grimtol_23;
+    src: url(/assets/grimtol_23-7d80159c.eot?9ep1bj);
+    src: url(/assets/grimtol_23-7d80159c.eot?9ep1bj#iefix) format("embedded-opentype"),url(/assets/grimtol_23-0c4b637f.ttf?9ep1bj) format("truetype"),url(/assets/grimtol_23-c6fd7f9b.woff?9ep1bj) format("woff"),url(/assets/grimtol_23-460adf95.svg?9ep1bj#grimtol_23) format("svg");
+    font-weight: 400;
+    font-style: normal;
+    font-display: block
+}
+
+.metalMania{
+  font-family: 'Metal Mania';
+  color:white;
+}
+
+.robotoMono{
+  color:white;
+  font-family: 'Roboto Mono';
+}
+
+.bgBlur{
+    backdrop-filter: blur(2px);
+    border: 1px white
+}
+
+.charSelectBG{
+  height: 100%;
+  width: 100%;
+  background-color: #123456;
+  background-image: url('../assets/img/grimtolTownWideLowContrast.png');
+  background-position: center;
+  background-size: cover;
+}
 
 </style>
