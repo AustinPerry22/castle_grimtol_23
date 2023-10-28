1dbbef16c352673b3c40aa7e7e8eb6c73c86f147
Austin <austinjperry22@gmail.com>
2023-10-28 14:40:07 -0600

    map

--- HalloweenHackathon.client/src/pages/MapPage.vue
+++ HalloweenHackathon.client/src/pages/MapPage.vue
@@ -4,6 +4,7 @@
     Scare St.
 </h1>
 <img src="https://media3.giphy.com/media/wiwZ3kBPtrG6wYMNjw/giphy.gif?cid=6c09b952bkudwaf55l5px1ksankpvji0zba4f6ctk5gvqi9n&ep=v1_stickers_related&rid=giphy.gif&ct=s" alt="" class="house1">
+<img src="../assets/img/lock.png" alt="" class="house5 lock-img">
 <img src="https://media4.giphy.com/media/TWCf83eMB06aHrBDNC/200w.gif?cid=6c09b9522wdfv6nczxilulxl8od0ci89z6llvtgldzdljc2c&ep=v1_gifs_search&rid=200w.gif&ct=g" alt="" class="house2">
 <img src="https://media3.giphy.com/media/wiwZ3kBPtrG6wYMNjw/giphy.gif?cid=6c09b952bkudwaf55l5px1ksankpvji0zba4f6ctk5gvqi9n&ep=v1_stickers_related&rid=giphy.gif&ct=s" alt="" class="house3">
 <img src="https://media3.giphy.com/media/wiwZ3kBPtrG6wYMNjw/giphy.gif?cid=6c09b952bkudwaf55l5px1ksankpvji0zba4f6ctk5gvqi9n&ep=v1_stickers_related&rid=giphy.gif&ct=s" alt="" class="house4">
@@ -28,6 +29,12 @@ export default {
     height: 100dvh;
     width: 100dvw;
     z-index: -2000;
+}
+.lock-img{
+    width: 15%;
+    height: 30%;
+    border-radius: 5rem;
+    z-index: 1;
 }
 .house1{
     position: absolute;
