edb8acb02b19d6a6699ecde5d3c9e902933c569f
Austin <austinjperry22@gmail.com>
2023-10-28 18:21:07 -0600

    able to flip cards

--- HalloweenHackathon.client/src/pages/TriviaGrid.vue
+++ HalloweenHackathon.client/src/pages/TriviaGrid.vue
@@ -2,18 +2,17 @@
     <img v-if="jumpscare" src="https://www.horrormovietalk.com/wp-content/uploads/2018/11/The-Nun-Jump-Scare.jpg" alt="" class="scare-card">
     <div class="container-fluid">
         <div class="row p-0 m-1 d-flex justify-content-between">
-            <div v-for="card in cards" :key="card.id" @click="getCard(card)" class="col-4 card text-white flex justify-content-center align-items-center my-2 selectable">
-                <div class="fs-3 fw-bold">
-                    <div v-if="card.type == 0 && !card.locked">
-                        <QuestionCard/>
-                    </div>
-                    <div v-if="card.type == 1 && !card.locked">
-                        health potion
-                    </div>
-                    <div v-if="card.type == 2 ">
-                        suprise
-                    </div>
+            <div v-for="card in cards" :key="card.id" @click="getCard(card)" class="col-4">
+                <div v-if="card.type == 0 && !card.locked">
+                    <TriviaCard/>
                 </div>
+                <div v-if="card.type == 1 && !card.locked">
+                    spooky
+                </div>
+                <div v-if="card.type == 2 && !card.locked">
+                    hi
+                </div>
+                <div v-if="card.locked" class="card text-white flex justify-content-center align-items-center my-2 selectable"></div>
             </div>
         </div>
     </div>
@@ -26,27 +25,23 @@ import { AppState } from '../AppState';
 import { computed, reactive, onMounted } from 'vue';
 import {triviaService} from '../services/TriviaService'
 import { logger } from '../utils/Logger';
+import ScareCard from '../components/ScareCard.vue';
+import TriviaCard from '../components/TriviaCard.vue';
 export default {
-    setup(){
-        onMounted(()=>
-            generateCards()
-        )
-        function generateCards()
-        {
-            triviaService.generateCards()
-            logger.log("trivia cards", AppState.triviaCards)
+    setup() {
+        onMounted(() => generateCards());
+        function generateCards() {
+            triviaService.generateCards();
         }
-    return { 
-        jumpscare: computed(()=> AppState.jumpscare),
-        cards: computed(()=> AppState.triviaCards),
-
-        getCard(card) {
-            card.locked = false;
-            logger.log(card.locked)
-            logger.log(AppState.triviaCards)
-        }
-        }
-    }
+        return {
+            jumpscare: computed(() => AppState.jumpscare),
+            cards: computed(() => AppState.triviaCards),
+            getCard(card) {
+                card.locked = false;
+            }
+        };
+    },
+    components: { ScareCard, TriviaCard }
 };
 </script>
 
@@ -61,7 +56,6 @@ export default {
     background-image: url("https://st4.depositphotos.com/2627021/31189/i/450/depositphotos_311899432-stock-photo-jack-lanterns-spooky-forest-moonlight.jpg") ;
     background-size: cover;
     background-position: center;
-    
 }
 
 
