ca370f392355f9550cae806dd54bdc606b752b05
DJ Aaron <koreangeekman@gmail.com>
2023-12-06 11:22:18 -0700

    tweak character select

--- HalloweenHackathon.client/src/pages/CharacterSelect.vue
+++ HalloweenHackathon.client/src/pages/CharacterSelect.vue
@@ -6,7 +6,7 @@
       </div>
     </section>
     <section v-for="char in characters" :key="char.id" @click="selectCharacter(char)" 
-      class="row border rounded p-1 p-md-3 m-1 m-md-3 justify-content-center" type="button" title="Click to Choose Character">
+      class="row border rounded p-1 p-md-3 m-1 m-md-3 justify-content-center selectable darken-10" type="button" :title="'Click to Choose '+ char.name">
       <div class="col-12 col-md-2 p-1 p-md-3 d-flex justify-content-end" >
         <img :src="char.picture" :alt="char.name" class="rounded">
       </div>
@@ -22,17 +22,18 @@
     <section class="row p-2 p-md-5">
         <div class="col-12">
           <p class="fs-1 metalMania text-center mt-3">You have selected:</p>
+          <p class="fs-3 metalMania text-center">the {{ player.character.name }}</p>
         </div>
       </section>
     <section class="row border rounded p-1 p-md-3 m-1 m-md-3 justify-content-center">
       <div class="col-12 col-md-2 p-3 d-flex justify-content-end">
-        <img :src="player.character.picture" :alt="player.character.name" class="rounded">
+        <img :src="player.character.picture" :alt="player.character.name" :title="player.character.name" class="rounded">
       </div>
       <div class="col-12 col-md-6 p-1 p-md-3 d-flex align-items-center">
         <p class="robotoMono p-1 p-md-3 m-1 m-md-3 rounded bgBlur">{{ player.character.description }}</p>
       </div>
       <div class="col-12 col-md-2 p-1 p-md-3 d-flex justify-content-start">
-        <img :src="player.character.altPicture" :alt="player.character.altName" class="rounded">
+        <img :src="player.character.altPicture" :alt="player.character.altName" :title="player.character.altName" class="rounded">
       </div>
     </section>
     <section class="row p-2 p-md-5">
@@ -78,17 +79,6 @@ export default {
 
 <style lang="scss" scoped>
 
-@charset "UTF-8";
-@import"https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,400;0,600;1,400&family=Metal+Mania&family=VT323&display=swap";
-@font-face {
-    font-family: grimtol_23;
-    src: url(/assets/grimtol_23-7d80159c.eot?9ep1bj);
-    src: url(/assets/grimtol_23-7d80159c.eot?9ep1bj#iefix) format("embedded-opentype"),url(/assets/grimtol_23-0c4b637f.ttf?9ep1bj) format("truetype"),url(/assets/grimtol_23-c6fd7f9b.woff?9ep1bj) format("woff"),url(/assets/grimtol_23-460adf95.svg?9ep1bj#grimtol_23) format("svg");
-    font-weight: 400;
-    font-style: normal;
-    font-display: block
-}
-
 .metalMania{
   font-family: 'Metal Mania';
   color:white;
@@ -106,6 +96,14 @@ img{
 .bgBlur{
     backdrop-filter: blur(2px);
     border: 1px white
+}
+
+.selectable{
+  transform: scale(97%);
+  transition: .25s;
+}
+.selectable:hover{
+  transform: scale(100%);
 }
 
 .charSelectBG{
