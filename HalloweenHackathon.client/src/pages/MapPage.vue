5b7903c87228c0e5586de2e621d8f003fc3166a1
DJ Aaron <koreangeekman@gmail.com>
2023-12-06 11:52:28 -0700

    map page tweaks

--- HalloweenHackathon.client/src/pages/MapPage.vue
+++ HalloweenHackathon.client/src/pages/MapPage.vue
@@ -1,29 +1,46 @@
 <template>
     <img class="map-img" src="../assets/img/map.png" alt="">
-    <h1 class="title">
-        Scare St.
-    </h1>
-    <img v-if="house1Locked" src="../assets/img/lock.png" alt="" class="house1 lock-img">
-    <img v-if="house2Locked" src="../assets/img/lock.png" alt="" class="house2 lock-img">
-    <img v-if="house3Locked" src="../assets/img/lock.png" alt="" class="house3 lock-img">
-    <img v-if="house4Locked" src="../assets/img/lock.png" alt="" class="house4 lock-img">
-    <img v-if="house5Locked" src="../assets/img/lock.png" alt="" class="house5 lock-img">
-    <img src="https://media3.giphy.com/media/wiwZ3kBPtrG6wYMNjw/giphy.gif?cid=6c09b952bkudwaf55l5px1ksankpvji0zba4f6ctk5gvqi9n&ep=v1_stickers_related&rid=giphy.gif&ct=s" alt="" class="house1" @click="goToHousePage(1, house1Locked)">
-    <img src="https://media4.giphy.com/media/dOTLg5cVzgMap6na2l/200.webp?cid=ecf05e47qwf06g0dx80rj9th54tufrx7k6btouu1og4n9tvk&ep=v1_stickers_search&rid=200.webp&ct=s" alt="" class="house2" @click="goToHousePage(2, house2Locked)">
-    <img src="https://media3.giphy.com/media/sDkA2qix4cnK5P8BUZ/200.webp?cid=ecf05e47howgjdzp6ovy3yskbx8p83d7mrqju3vv0qabhc2r&ep=v1_stickers_search&rid=200.webp&ct=s" alt="" class="house3" @click="goToHousePage(3, house3Locked)">
-    <img src="https://media4.giphy.com/media/vWoRDgGYaZAqw5jzwR/200.webp?cid=ecf05e47yez34kroushgynwgzz015nbt4v43bzvq753gg3yo&ep=v1_stickers_search&rid=200.webp&ct=s" alt="" class="house4" @click="goToHousePage(4, house4Locked)">
-    <img src="https://media0.giphy.com/media/6DOkW5jyFFdQVa5iQT/200.webp?cid=ecf05e47yez34kroushgynwgzz015nbt4v43bzvq753gg3yo&ep=v1_stickers_search&rid=200.webp&ct=s" alt="" class="house5" @click="goToHousePage(5, house5Locked)">
+    <h1 class="title"> Scare St. </h1>
     <ScoreboardButton />
-    </template>
-    
-    
-    <script>
-    import { AppState } from '../AppState';
-    import { computed, reactive, onMounted } from 'vue';
-    import { router } from '../router';
-    import ScoreboardButton from "../components/ScoreboardButton.vue";
+    <div class="position-relative">
 
-    export default {
+        <div class="house1 btn">1
+            <!-- <img v-if="house1Locked" src="../assets/img/lock.png" alt="1" class="lock-img"> -->
+            <img src="https://media3.giphy.com/media/wiwZ3kBPtrG6wYMNjw/giphy.gif?cid=6c09b952bkudwaf55l5px1ksankpvji0zba4f6ctk5gvqi9n&ep=v1_stickers_related&rid=giphy.gif&ct=s"
+                alt="1" class="" @click="goToHousePage(1, house1Locked)">
+        </div>
+        <div class="house2 btn">2
+            <img v-if="house2Locked" src="../assets/img/lock.png" alt="2" class="lock-img">
+            <img src="https://media4.giphy.com/media/dOTLg5cVzgMap6na2l/200.webp?cid=ecf05e47qwf06g0dx80rj9th54tufrx7k6btouu1og4n9tvk&ep=v1_stickers_search&rid=200.webp&ct=s"
+                alt="2" class="" @click="goToHousePage(2, house2Locked)">
+        </div>
+        <div class="house3 btn">3
+            <img v-if="house3Locked" src="../assets/img/lock.png" alt="3" class="lock-img">
+            <img src="https://media3.giphy.com/media/sDkA2qix4cnK5P8BUZ/200.webp?cid=ecf05e47howgjdzp6ovy3yskbx8p83d7mrqju3vv0qabhc2r&ep=v1_stickers_search&rid=200.webp&ct=s"
+                alt="3" class="" @click="goToHousePage(3, house3Locked)">
+        </div>
+        <div class="house4 btn">4
+            <img v-if="house4Locked" src="../assets/img/lock.png" alt="4" class="lock-img">
+            <img src="https://media4.giphy.com/media/vWoRDgGYaZAqw5jzwR/200.webp?cid=ecf05e47yez34kroushgynwgzz015nbt4v43bzvq753gg3yo&ep=v1_stickers_search&rid=200.webp&ct=s"
+                alt="4" class="" @click="goToHousePage(4, house4Locked)">
+        </div>
+        <div class="house5 btn">5
+            <img v-if="house5Locked" src="../assets/img/lock.png" alt="5" class="lock-img">
+            <img src="https://media0.giphy.com/media/6DOkW5jyFFdQVa5iQT/200.webp?cid=ecf05e47yez34kroushgynwgzz015nbt4v43bzvq753gg3yo&ep=v1_stickers_search&rid=200.webp&ct=s"
+                alt="5" class="" @click="goToHousePage(5, house5Locked)">
+        </div>
+
+    </div>
+</template>
+    
+    
+<script>
+import { AppState } from '../AppState';
+import { computed } from 'vue';
+import { router } from '../router';
+import ScoreboardButton from "../components/ScoreboardButton.vue";
+
+export default {
     setup() {
         return {
             house1Locked: computed(() => AppState.house1Locked),
@@ -40,52 +57,69 @@
     },
     components: { ScoreboardButton }
 };
-    </script>
-    
-    
-    <style lang="scss" scoped>
-    .map-img{
-        position: absolute;
-        height: 100dvh;
-        width: 100dvw;
-        z-index: -2000;
-    }
-    .lock-img{
-    width: 15%;
-    height: 30%;
-    border-radius: 5rem;
+</script>
+
+
+<style lang="scss" scoped>
+// img { border: 1px solid greenyellow;}
+img{
+    transition: .25s;
+}
+img:hover{
+    transform: scale(105%);
+    border-radius: 100%;
+    box-shadow:
+    0 0 25px 5px #80808080,
+    0 0 75px 5px #80808080 inset;
+}
+
+.map-img {
+    position: absolute;
+    height: 100dvh;
+    width: 100dvw;
+    z-index: -2000;
+}
+
+.lock-img {
+    position: inherit;
+    height: 94%;
+    width: 84%;
+    border-radius: 8rem;
     z-index: 1;
 }
-.house1{
+
+.house1 {
     position: absolute;
-    top: 45dvh;
+    top: 42dvh;
     left: 5dvw;
 }
-.house2{
+
+.house2 {
     position: absolute;
-    border-radius: 5rem;
-    width: 12.5dvw;
     top: 10dvh;
     left: 10dvw;
 }
-.house3{
+
+.house3 {
     position: absolute;
     top: 37.5dvh;
     left: 35dvw;
 }
-.house4{
+
+.house4 {
     position: absolute;
     top: 60dvh;
     left: 67.5dvw;
 }
-.house5{
+
+.house5 {
     position: absolute;
     top: 2.5dvh;
     left: 55dvw;
 }
-.title{
+
+.title {
     font-size: 70px;
     color: rgba(174, 10, 10, 0.734);
     font-family: 'Metal Mania';
-  }
-</style>
+}</style>
